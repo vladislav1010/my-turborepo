72a3696f9a96c64aff8def53d8fbea7e239bb818
Владислав Савченко <savchenkovs1@yandex.ru>
2022-12-19 17:25:31 +0300

    wip

--- packages/personal-navigation/ui/index.tsx
+++ packages/personal-navigation/ui/index.tsx
@@ -11,6 +11,7 @@ import { ChecklistItem } from "checklist";
 export function PersonalNavigation({ className }: { className: string }) {
   return (
     <div className={clsx(className, s.root)}>
+      {/* $4 В этом мне помогают явные зависимости / знание, какие динамические зависимости могут быть. Например, в типах ответа серверного API есть тип, подразумевающий альтернативу ChecklistItem как ItemComponent. Другой вопрос, что мне нужно читать больше легко обнаруживаемого и понятного (Discoverability and understanding) кода. */}
       <Cart className={""} items={[]} ItemComponent={ChecklistItem} />
     </div>
   );
